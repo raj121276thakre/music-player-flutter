6f293e7df332911e5f22bcc5d1f399af06f19fe4
raj121276thakre <raj121276thakre@gmail.com>
2023-12-17 17:09:24 +0000

    Code Synced by DhiWise

---
+++ android/app/src/main/kotlin/com/rajsapplication/app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.rajsapplication.app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/data/apiClient/api_client.dart
@@ -0,0 +1,3 @@
+import 'package:raj_s_application2/core/app_export.dart';
+
+class ApiClient {}
